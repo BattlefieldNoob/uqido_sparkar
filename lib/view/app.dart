da12b31f4dfdbcc0e2607ae3ec88dbb25d0a310e
Antonio Ruggiero <ant.ruggiero@gmail.com>
2020-12-27 18:38:13 +0100

    Added first version on Master-Detail

--- lib/view/app.dart
+++ lib/view/app.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,6 +7,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_hooks_bloc/flutter_hooks_bloc.dart';
 import 'package:responsive_framework/responsive_wrapper.dart';
 import 'package:responsive_framework/utils/scroll_behavior.dart';
+import 'package:responsive_scaffold/templates/list/responsive_list.dart';
 import 'package:uqido_sparkar/blocs/sparkar_bloc.dart';
 import 'package:uqido_sparkar/utils/string_extension.dart';
 import 'package:uqido_sparkar/view/widgets/search_app_bar.dart';
@@ -17,89 +20,144 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        builder: (context, widget) => ResponsiveWrapper.builder(
-                BouncingScrollWrapper.builder(context, widget),
-                breakpoints: [
-                  ResponsiveBreakpoint.resize(450, name: MOBILE),
-                  ResponsiveBreakpoint.autoScale(800,
-                      scaleFactor: 0.8, name: TABLET),
-                  ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-                ]),
-        theme: ThemeData(
-            brightness: Brightness.dark,
-            scaffoldBackgroundColor: Color.fromRGBO(38, 44, 50, .9),
-            accentColor: Color.fromRGBO(78, 86, 176, 1.0)),
-        home: BlocProvider(
-            create: (_) => SparkARBloc(),
-            child: Builder(builder: (ctx) {
-              return HookBuilder(builder: (ctx) {
-                print('Body HookBuilder');
-                final state = useBloc<SparkARBloc, SparkARState>(
-                  onEmitted: (_, prev, curr) {
-                    print(curr);
-                    //return prev.userList != curr.userList;
-                    return true;
-                  },
-                ).state;
+    return BlocProvider(
+        create: (_) => SparkARBloc(),
+        child: MaterialApp(
+            routes: {
+              '/': (context) => generateHome(),
+              '/test': (context) => generateMasterDetail()
+            },
+            initialRoute: '/',
+            builder: (context, widget) => ResponsiveWrapper.builder(
+                    BouncingScrollWrapper.builder(context, widget),
+                    breakpoints: [
+                      ResponsiveBreakpoint.resize(450, name: MOBILE),
+                      ResponsiveBreakpoint.autoScale(800,
+                          scaleFactor: 0.8, name: TABLET),
+                      ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+                    ]),
+            theme: ThemeData.dark().copyWith(
+                scaffoldBackgroundColor: Color.fromRGBO(38, 44, 50, .9),
+                backgroundColor: Color.fromRGBO(30, 38, 45, .9),
+                accentColor: Color.fromRGBO(78, 86, 176, 1.0))));
+  }
 
-                if (state.userList.length == 0) return SizedBox();
+  Widget generateHome() {
+    return Builder(builder: (ctx) {
+      return HookBuilder(builder: (ctx) {
+        print('Body HookBuilder');
+        final state = useBloc<SparkARBloc, SparkARState>(
+          onEmitted: (_, prev, curr) {
+            print(curr);
+            //return prev.userList != curr.userList;
+            return true;
+          },
+        ).state;
 
-                return NavRail(
-                    drawerHeaderBuilder: (context) {
-                      return Column(
-                        children: <Widget>[],
-                      );
-                    },
-                    title: const Text('Uqido Spark AR'),
-                    appBar: SearchAppBar(
-                        primary: Theme.of(ctx).primaryColor,
-                        searchHint: "Cerca un effetto...",
-                        mainTextColor: Colors.white,
-                        onSubmit: (String value) {
-                          print(value);
-                          ctx
-                              .read<SparkARBloc>()
-                              .add(SparkARSearchAction(value));
-                        },
-                        //Will show when SEARCH MODE wasn't active
-                        mainAppBar: (stream, keyword) {
-                          return AppBar(
-                              title: const Text('Uqido Spark AR'),
-                              actions: [
-                                IconButton(
-                                  onPressed: () {
-                                    stream.add(true);
-                                  },
-                                  icon: const Icon(Icons.search),
-                                ),
-                                const SizedBox(
-                                  width: 10,
-                                )
-                              ],
-                              bottom: getAppBarLoadingBar());
-                        }),
-                    /*floatingActionButton: FloatingActionButton(
-                      onPressed: () {},
-                      child: Icon(Icons.search),
-                    ),*/
-                    onTap: (index) {
-                      ctx
-                          .read<SparkARBloc>()
-                          .add(SparkARSelectUserAction(index));
-                    },
-                    currentIndex: state.selectedIndex,
-                    tabs: state.userList
-                        .map((e) => BottomNavigationBarItem(
-                            icon: Image.network(e.iconUrl),
-                            label: e.name.toLowerCapitalize()))
-                        .toList(),
-                    body: Container(
-                        color: const Color.fromRGBO(50, 58, 78, 1.0),
-                        child: UserEffectDetail(
-                            state.userList[state.selectedIndex])));
-              });
-            })));
+        if (state.userList.length == 0) return SizedBox();
+
+        return NavRail(
+            drawerHeaderBuilder: (context) {
+              return Column(
+                children: <Widget>[],
+              );
+            },
+            title: const Text('Uqido Spark AR'),
+            appBar: generateSearchAppBar(ctx),
+            /*floatingActionButton: FloatingActionButton(
+                    onPressed: () {},
+                    child: Icon(Icons.search),
+                  ),*/
+            onTap: (index) {
+              ctx.read<SparkARBloc>().add(SparkARSelectUserAction(index));
+            },
+            currentIndex: state.selectedIndex,
+            tabs: state.userList
+                .map((e) => BottomNavigationBarItem(
+                    icon: Image.network(e.iconUrl),
+                    label: e.name.toLowerCapitalize()))
+                .toList(),
+            body: UserEffectDetail(state.userList[state.selectedIndex]));
+      });
+    });
+  }
+
+  SearchAppBar generateSearchAppBar(BuildContext ctx) {
+    return SearchAppBar(
+        primary: Theme.of(ctx).primaryColor,
+        searchHint: "Cerca un effetto...",
+        mainTextColor: Colors.white,
+        onSubmit: (String value) {
+          print(value);
+          ctx.read<SparkARBloc>().add(SparkARSearchAction(value));
+        },
+        //Will show when SEARCH MODE wasn't active
+        mainAppBar: (stream, keyword) {
+          return generateMainAppBar(stream);
+        });
+  }
+
+  AppBar generateMainAppBar(StreamController<bool> stream) {
+    return AppBar(
+        title: const Text('Uqido Spark AR'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              stream.add(true);
+            },
+            icon: const Icon(Icons.search),
+          ),
+          const SizedBox(
+            width: 10,
+          )
+        ],
+        automaticallyImplyLeading: false,
+        bottom: getAppBarLoadingBar());
+  }
+
+  Widget generateMasterDetail() {
+    return Builder(builder: (ctx) {
+      return HookBuilder(builder: (ctx) {
+        print('Body HookBuilder');
+        final state = useBloc<SparkARBloc, SparkARState>(
+          onEmitted: (_, prev, curr) {
+            print(curr);
+            //return prev.userList != curr.userList;
+            return true;
+          },
+        ).state;
+
+        if (state.userList.length == 0) return SizedBox();
+
+        return ResponsiveListScaffold.builder(
+          detailBuilder: (context, index, tablet) {
+            return DetailsScreen(
+                body: Scaffold(
+                    appBar: generateSearchAppBar(ctx),
+                    body: UserEffectDetail(state.userList[index])));
+          },
+          nullItems: Center(child: CircularProgressIndicator()),
+          emptyItems: Center(child: Text("No Items Found")),
+          appBar: AppBar(
+            title: const Text("Uqido SparkAR"),
+          ),
+          tabletItemNotSelected: Center(child: Text("Seleziona un account")),
+          itemCount: state.userList.length,
+          backgroundColor: Theme.of(ctx).backgroundColor,
+          itemBuilder: (context, index) {
+            var current = state.userList[index];
+            return ListTile(
+                title: Text(current.name),
+                subtitle: Text("${current.effects.length} effetti"),
+                leading: Image.network(
+                  current.iconUrl,
+                  width: 48,
+                  height: 48,
+                ));
+          },
+        );
+      });
+    });
   }
 
   PreferredSize getAppBarLoadingBar() {
